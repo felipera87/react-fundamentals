d06d2f2997e9bb4ade6fc311cb87ed5c88170a9d
felipera8732 <felipe@pruvo.com>
2023-03-16 21:24:28 -0300

    More sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,15 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/felipera87.png" />
+
         <strong>Felipe Reis</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Edit profile
         </a>
       </footer>
