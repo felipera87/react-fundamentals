ff417dba6a863308bddfc82ce257e1ae1ce2926a
felipera8732 <felipe@pruvo.com>
2023-03-16 21:10:15 -0300

    Added sidebar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar"
+
 import './global.css';
+
+import styles from './App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello world</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          posts
+        </main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1678697598253-76b3e913e64b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=60"
+      />
+
+      <div className={styles.profile}>
+        <strong>Felipe Reis</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Edit profile
+        </a>
+      </footer>
+    </aside>
+  )
+}
