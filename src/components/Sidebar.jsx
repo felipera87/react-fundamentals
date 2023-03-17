24965c552b2502a5b38b8968d0605514fb6baf40
felipera8732 <felipe@pruvo.com>
2023-03-17 14:25:36 -0300

    Added Avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,7 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/felipera87.png" />
+        <Avatar src="https://github.com/felipera87.png" />
 
         <strong>Felipe Reis</strong>
         <span>Web Developer</span>
