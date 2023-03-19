a13beae81e5b8f85191df0fd81914d0a99402884
felipera8732 <felipe@pruvo.com>
2023-03-19 17:48:15 -0300

    Fix Post type

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header";
-import { Sidebar } from "./components/Sidebar"
-import { Post } from "./components/Post"
+import { Sidebar } from "./components/Sidebar";
+import { Post, PostType } from "./components/Post";
 
 import './global.css';
 
 import styles from './App.module.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -51,9 +51,7 @@ export function App() {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             );
           })}
